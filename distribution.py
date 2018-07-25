0735395a942773a9b19670291c1d049d94ff47ed
CWHSU <chiaweh2@uci.edu>
2018-07-25 11:54:56 -0700

    add the docstring in the distribution.py

--- distribution.py
+++ distribution.py
@@ -1,10 +1,44 @@
 import scipy.stats
 import sys
 class ci2nstd:
+    """
+        Input confidence interval "ci" in %, degree of freedoms,
+        and number of tails in the designed test.
+        
+        The program will output the critical value alpha and the
+        associated number of standard deviation based on the method picked 
+        (different distribution)
+    """
     def __init__(self) :
         return
     
     def t_dist(self, ci, dof, ntail=2):
+        """
+        Input confidence interval "ci" in %, degree of freedoms,
+        and number of tails in the designed test.
+        
+        The program will output the critical value alpha and the
+        associated number of standard deviation based on the t-dist.
+
+        Parameters:
+            ci : numpy float
+                represents the confidence interval in %
+            dof : numpy int
+                represents the degree of freedom in the test
+            ntail: kwarg, numpy int, with only two options (1 or 2)
+                number of tails in the designed test
+        
+        Returns: 
+            nstd : numpy float
+                associated number of standard deviation based on the t-dist
+                
+        Attributes:
+            self.alpha
+            self.nstd
+            self.dof
+            self.ntail
+
+        """
         if ntail != 1 and ntail != 2 :
             print "ntail can only be 1 or 2 "
             sys.exit('')
